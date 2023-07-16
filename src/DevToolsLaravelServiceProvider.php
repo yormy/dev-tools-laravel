ec87ff849a7bde8d634b238f8ca2700dc14a634c
Mexion <dev@mexion.com>
2023-07-16 17:15:02 +0200

    remove dummy

--- src/DevToolsLaravelServiceProvider.php
+++ src/DevToolsLaravelServiceProvider.php
@@ -6,8 +6,5 @@ use Illuminate\Support\ServiceProvider;
 
 class DevToolsLaravelServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/assert-laravel.php', 'assert-laravel');
-    }
+    // ...
 }
