80efe0753af1ef38d1b684b297717aa1c3fddec9
Mexion <dev@mexion.com>
2025-01-01 11:22:09 +0100

    clean: pint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,4 @@ namespace Yormy\DevToolsLaravel\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
-}
+abstract class TestCase extends BaseTestCase {}
